a2e92045620f187dfc6ff0729b7b323119ab30cb
Eduard Bucari <eduard.bucari@gmail.com>
2022-02-04 02:44:16 +0300

    3. LazyInitializationexception [#331987]

---
+++ src/main/java/ru/job4j/lazy/cars/CarsRun.java
@@ -0,0 +1,34 @@
+package ru.job4j.lazy.cars;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsRun {
+    public static void main(String[] args) {
+        List<Mark> list = new ArrayList<>();
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            list = session.createQuery(
+                    "select distinct m from Mark m join fetch m.models").list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+        for (Model model : list.get(0).getModels()) {
+            System.out.println(model);
+        }
+    }
+}
